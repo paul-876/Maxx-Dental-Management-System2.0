48b615c08d89e4ce02d4326a055bd64a8c35850d
batman-spec <paulpere001@gmail.com>
2026-09-16 20:11:26 +0300

    Use PostgreSQL DATABASE_URL

--- src/config/database.js
+++ src/config/database.js
@@ -1,19 +1,13 @@
 const { Sequelize } = require('sequelize');
+
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
-    logging: false,
-    define: {
-      underscored: true,
-    },
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  logging: false,
+  define: {
+    underscored: true,
+  },
+});
 
 module.exports = sequelize;
