e5cd76cbbe28a6bc99dffb55c718d6420e0d165e
batman-spec <paulpere001@gmail.com>
2026-09-16 20:17:02 +0300

    Enable PostgreSQL SSL

--- src/config/database.js
+++ src/config/database.js
@@ -5,6 +5,14 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   logging: false,
+
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+
   define: {
     underscored: true,
   },
